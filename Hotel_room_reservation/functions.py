aa10a24a3f04fecd5ca817cba93468f92bd03814
Matejic25 <nikolamatejic2501@gmail.com>
2022-01-14 16:08:06 +0100

    Added function 'proveri_termine_sobe' and implemented in function 'dodaj_rezervaciju'.  Order of functions changed because of the nesting.  Also, deleted some comments so code looks a bit cleaner.

--- Hotel_room_reservation/functions.py
+++ Hotel_room_reservation/functions.py
@@ -68,7 +68,6 @@ def promeni_rasp_sobe(id_sobe, zahtev):
 # print(promeni_rasp_sobe(3, 'zauzmi'))
 
 
-
 # TREBA
 # PROVERA KAPACITETA SOBE I BROJ ZAHTEVANIH OSOBA U SOBI
 
@@ -81,41 +80,31 @@ def provera_kapaciteta(id_sobe,osobe):
     return osobe <= kapacitet
 
 
-# Problem sa argumentima, ostalo radi
-# DODAVANJE REZERVACIJE NAKON ISPUNJENIH USLOVA
+# PROVERA DA LI SE ZELJENI DATUM ZA REZERVACIJU SOBE NALAZI U OPSEGU VEC REZERVISANIH SOBA
+# ako je soba zauzeta tokom zeljenog termina vraca False
+# ako je soba slobodna tokom zeljenog termina vraca True
 
 
-def dodaj_rezervaciju(id_sobe, broj_osoba):
-    if provera_kapaciteta(id_sobe, broj_osoba) is True:
-        if promeni_rasp_sobe(id_sobe, "zauzmi") != "vec_zauzeta":
-            mycursor.execute("INSERT INTO rezervacija(id_sobe, datum_rezervacije, broj_osoba,"
-                             " zakazani_datum_pocetka, zakazani_datum_zavrsetka,"
-                             " realni_datum_pocetka, realni_datum_zavrsetka)"
-                             "VALUES (%s,'2020-5-15',%s,'2020-5-15','2020-5-25','2020-5-15','2020-5-25')",
-                             (id_sobe, broj_osoba))
-            print("Rezervacija dodata.")
-            br_rasp_u_kategoriji()
-            connection.commit()
-        else:
-            print("soba vec zauzeta!")
-    else:
-        print("nema mesta u sobi!")
-
-
-# dodaj_rezervaciju(10, 2)
-
-
-def obrisi_rezervaciju(id_rezervacije):
-    id_sobe = None
-    mycursor.execute("SELECT id_sobe FROM rezervacija WHERE id_rezervacije=%s", id_rezervacije)
+def proveri_termine_sobe(id_sobe, datum_pocetka, datum_zavrsetka):
+    mycursor.execute("SELECT id_rezervacije, zakazani_datum_pocetka, zakazani_datum_zavrsetka FROM rezervacija"
+                     " WHERE zakazani_datum_pocetka BETWEEN %s"
+                     " AND %s AND id_sobe = %s OR zakazani_datum_zavrsetka BETWEEN"
+                     " %s AND %s AND id_sobe = %s"
+                     , (datum_pocetka, datum_zavrsetka, id_sobe, datum_pocetka, datum_zavrsetka, id_sobe))
+    rezultati = 0
     for i in mycursor:
-        id_sobe = i[0]
-    promeni_rasp_sobe(id_sobe, "oslobodi")
-    mycursor.execute("DELETE FROM rezervacija WHERE id_rezervacije=%s", id_rezervacije)
-    connection.commit()
-    print("Rezervacija obrisana.")
+        rezultati += 1
+        if rezultati > 0:
+            print(f"Soba: {id_sobe} je zauzeta | OD: {i[1]} | DO: {i[2]} ---- ID_Rezervacije: {i[0]}")
+        else:
+            print(f"Soba: {id_sobe} je slobodna za rezervaciju u terminu | OD: {datum_pocetka} | DO: {datum_zavrsetka}")
+    if rezultati > 0:
+        return False
+    else:
+        return True
 
-# obrisi_rezervaciju(8)
+
+# proveri_termine_sobe(3, '2020-5-13', '2020-5-26')
 
 
 # ZAVISNOST RASPOLOZIVOSTI SOBE U ODNOSU NA TO DA LI JE id_sobe U rezervacija
@@ -146,42 +135,39 @@ def raspolozivost_po_rezervacijama():
 
 # raspolozivost_po_rezervacijama()
 
-# DODAVANJE U TABELU OSOBE_U_REZERVACIJI I ISPIS REDOVA
-# mycursor.execute("INSERT INTO osobe_u_rezervaciji(kontakt_osoba, telefon) VALUES "
-#                  "(123456789,'063/555-333'),"
-#                  "(123456789,'063/555-333')")
-# connection.commit()
-# mycursor.execute("SELECT * FROM osobe_u_rezervaciji")
-# for i in mycursor:
-#     print(i)
+# DODAVANJE REZERVACIJE NAKON ISPUNJENIH USLOVA
+def dodaj_rezervaciju(id_sobe, broj_osoba, datum_pocetka, datum_zavrsetka):
+    if provera_kapaciteta(id_sobe, broj_osoba) is True:
+        if promeni_rasp_sobe(id_sobe, "zauzmi") != "vec_zauzeta" or\
+                promeni_rasp_sobe(id_sobe, "zauzmi") == "vec_zauzeta" and\
+                proveri_termine_sobe(id_sobe, datum_pocetka, datum_zavrsetka) is True:
+            mycursor.execute("INSERT INTO rezervacija(id_sobe, datum_rezervacije, broj_osoba,"
+                             " zakazani_datum_pocetka, zakazani_datum_zavrsetka,"
+                             " realni_datum_pocetka, realni_datum_zavrsetka)"
+                             "VALUES (%s,'2020-5-15',%s,'2020-5-15','2020-5-25','2020-5-15','2020-5-25')",
+                             (id_sobe, broj_osoba))
+            print("Rezervacija dodata.")
+            br_rasp_u_kategoriji()
+            raspolozivost_po_rezervacijama()
+            connection.commit()
+        else:
+            print("Rezervacije odbijena.")
+    else:
+        print("nema mesta u sobi.")
 
 
-# DODAVANJE U TABELU GOSTI I ISPIS REDOVA
-# mycursor.execute("INSERT INTO gosti(broj_pasosa, ime, prezime) VALUES "
-#                  "(123456789,'Nikola','Matejic'),"
-#                  "(9876543,'Nemanja','Nemanjic'),"
-#                  "(25252525,'Marko','Markovic'),"
-#                  "(19191919,'Petar','Petrovic')")
-# connection.commit()
-# mycursor.execute('SELECT * FROM gosti')
-# for i in mycursor:
-#     print(i)
+# dodaj_rezervaciju(10, 2, '2020-5-13', '2020-5-26')
 
 
-# DODAVANJE U TABELU SOBE I ISPIS REDOVA
-# for i in range(5):
-#     mycursor.execute('INSERT INTO sobe(sprat, id_kategorije, raspoloziva) VALUES (3, 1, 1)')
-#     connection.commit()
-# mycursor.execute('SELECT * FROM sobe')
-# for i in mycursor:
-#     print(i)
+def obrisi_rezervaciju(id_rezervacije):
+    id_sobe = None
+    mycursor.execute("SELECT id_sobe FROM rezervacija WHERE id_rezervacije=%s", id_rezervacije)
+    for i in mycursor:
+        id_sobe = i[0]
+    promeni_rasp_sobe(id_sobe, "oslobodi")
+    mycursor.execute("DELETE FROM rezervacija WHERE id_rezervacije=%s", id_rezervacije)
+    br_rasp_u_kategoriji()
+    connection.commit()
+    print("Rezervacija obrisana.")
 
-
-# DODAVANJE U TABELU KATEGORIJA_SOBA I ISPIS REDOVA
-# mycursor.execute("INSERT INTO kategorija_sobe(broj_kreveta, apartman, pogled, cena, ukupan_broj_soba, raspolozivo) "
-#                  "VALUES (4,TRUE,'more',100,5,5)")
-# connection.commit()
-#
-# mycursor.execute('SELECT * FROM kategorija_sobe')
-# for i in mycursor:
-#     print(i)
+# obrisi_rezervaciju(9)
