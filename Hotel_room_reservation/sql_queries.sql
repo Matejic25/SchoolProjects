d8aca517531e3cd07fed4b2efddeb43643a7a31f
Matejic25 <nikolamatejic2501@gmail.com>
2022-01-16 19:30:35 +0100

    Queries cleanup Queries that are not essential for database or tables creation are removed.

--- Hotel_room_reservation/sql_queries.sql
+++ Hotel_room_reservation/sql_queries.sql
@@ -1,108 +1,61 @@
 CREATE DATABASE hotel;
 USE hotel;
 
-CREATE TABLE rezervacija(
-	id_rezervacije INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    id_sobe INT NOT NULL,
-    datum_rezervacije DATE NOT NULL,
-    broj_osoba INT NOT NULL,
-    zakazani_datum_pocetka DATE NOT NULL,
-    zakazani_datum_zavrsetka DATE NOT NULL,
-    realni_datum_pocetka DATE,
-    realni_datum_zavrsetka DATE,
-    FOREIGN KEY (id_sobe) REFERENCES sobe(id_sobe)
-);
+CREATE TABLE `kategorija_sobe` (
+  `id_kategorije` int NOT NULL AUTO_INCREMENT,
+  `broj_kreveta` int NOT NULL,
+  `apartman` tinyint(1) NOT NULL,
+  `pogled` enum('more','parking','park','tereni') NOT NULL,
+  `cena` float NOT NULL,
+  `ukupan_broj_soba` int NOT NULL,
+  `raspolozivo` int NOT NULL,
+  PRIMARY KEY (`id_kategorije`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-SELECT id_kategorije FROM sobe WHERE id_sobe = 2;
-SELECT broj_kreveta FROM kategorija_sobe WHERE id_kategorije = 3;
-SELECT * FROM rezervacija;
+CREATE TABLE `sobe` (
+  `id_sobe` int NOT NULL AUTO_INCREMENT,
+  `sprat` int NOT NULL,
+  `id_kategorije` int NOT NULL,
+  `raspoloziva` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id_sobe`),
+  KEY `id_kategorije` (`id_kategorije`),
+  CONSTRAINT `sobe_ibfk_1` FOREIGN KEY (`id_kategorije`) REFERENCES `kategorija_sobe` (`id_kategorije`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-SELECT broj_kreveta
-FROM sobe AS s
-LEFT JOIN kategorija_sobe AS k
-ON s.id_kategorije = k.id_kategorije
-WHERE s.id_sobe = 2;
+CREATE TABLE `rezervacija` (
+  `id_rezervacije` int NOT NULL AUTO_INCREMENT,
+  `datum_rezervacije` date NOT NULL,
+  `broj_osoba` int NOT NULL,
+  `zakazani_datum_pocetka` date NOT NULL,
+  `zakazani_datum_zavrsetka` date NOT NULL,
+  `realni_datum_pocetka` date DEFAULT NULL,
+  `realni_datum_zavrsetka` date DEFAULT NULL,
+  `id_sobe` int NOT NULL,
+  PRIMARY KEY (`id_rezervacije`),
+  KEY `id_sobe` (`id_sobe`),
+  CONSTRAINT `rezervacija_ibfk_1` FOREIGN KEY (`id_sobe`) REFERENCES `sobe` (`id_sobe`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE sobe(
-	id_sobe INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    sprat INT NOT NULL,
-    id_kategorije INT NOT NULL,
-    raspoloziva BOOL NOT NULL,
-    FOREIGN KEY (id_kategorije) REFERENCES kategorija_sobe(id_kategorije)
-);
+CREATE TABLE `gosti` (
+  `id_gosta` int NOT NULL AUTO_INCREMENT,
+  `broj_pasosa` int NOT NULL,
+  `ime` varchar(50) NOT NULL,
+  `prezime` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_gosta`),
+  UNIQUE KEY `broj_pasosa` (`broj_pasosa`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE kategorija_sobe(
-	id_kategorije INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    broj_kreveta INT NOT NULL,
-    apartman BOOL NOT NULL,
-    pogled ENUM('more','parking','park','tereni'),
-    cena FLOAT NOT NULL,
-    ukupan_broj_soba INT NOT NULL,
-    raspolozivo INT
-);
-
-CREATE TABLE gosti(
-	id_gosta INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    broj_pasosa INT NOT NULL UNIQUE,
-    ime VARCHAR(50) NOT NULL,
-    prezime VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE osobe_u_rezervaciji(
-	kontakt_osoba INT NOT NULL,
-    telefon VARCHAR(20) NOT NULL,
-    FOREIGN KEY (kontakt_osoba) REFERENCES gosti(broj_pasosa)
-);
-
-
-ALTER TABLE sobe DROP foreign key id_kategorije;
-ALTER TABLE rezervacija ADD FOREIGN KEY (id_sobe) REFERENCES sobe(id_sobe);
-ALTER TABLE rezervacija ADD id_sobe INT NOT NULL;
-DROP TABLE sobe;
-SELECT * FROM sobe;
-SELECT * FROM kategorija_sobe;
-SELECT * FROM gosti;
-SELECT * FROM rezervacija;
-DELETE FROM rezervacija WHERE id_sobe = 0;
-SELECT id_kategorije FROM sobe;
-SELECT id_sobe FROM sobe;
-SELECT * FROM rezervacija;
-ALTER TABLE rezervacija DROP COLUMN id_sobe;
-SELECT sobe.id_sobe,sobe.id_kategorije
-FROM sobe
-WHERE sobe.id_kategorije = 3;
-
-SELECT sobe.id_sobe
-FROM sobe
-WHERE sobe.id_kategorije = 3;
-
-ALTER TABLE sobe ADD COLUMN raspoloziva BOOL NOT NULL;
-
-DELETE FROM rezervacija WHERE id_sobe = 7;
-
-SELECT sobe.id_sobe
-FROM sobe
-WHERE sobe.raspoloziva = 0;
-
-SELECT id_sobe FROM rezervacija;
-
-SELECT id_kategorije FROM sobe WHERE id_sobe = 5;
-INSERT INTO sobe(sprat,id_kategorije,raspoloziva) VALUES(1,2,1);
-
-UPDATE hotel.kategorija_sobe
- SET broj_kreveta = 4
-    ,apartman =  1
-    ,pogled = "more"
-    ,cena = 100
-    ,ukupan_broj_soba = 5
-    ,raspolozivo = 5
- WHERE id_kategorije = 1;
-
- UPDATE hotel.kategorija_sobe
- SET broj_kreveta = 3
-    ,apartman =  0
-    ,pogled = "tereni"
-    ,cena = 60
-    ,ukupan_broj_soba = 15
-    ,raspolozivo = 15
- WHERE id_kategorije = 3;
+CREATE TABLE `osobe_u_rezervaciji` (
+  `id_OuR` int NOT NULL AUTO_INCREMENT,
+  `id_gosta` int NOT NULL,
+  `kontakt_osoba` int NOT NULL,
+  `id_rezervacije` int NOT NULL,
+  `telefon` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_OuR`),
+  KEY `osobe_u_rezervaciji_ibfk_2_idx` (`id_gosta`),
+  KEY `osobe_u_rezervaciji_ibfk_3_idx` (`id_rezervacije`),
+  KEY `osobe_u_rezervaciji_ibfk_1_idx` (`kontakt_osoba`),
+  CONSTRAINT `osobe_u_rezervaciji_ibfk_1` FOREIGN KEY (`kontakt_osoba`) REFERENCES `gosti` (`broj_pasosa`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `osobe_u_rezervaciji_ibfk_2` FOREIGN KEY (`id_gosta`) REFERENCES `gosti` (`id_gosta`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `osobe_u_rezervaciji_ibfk_3` FOREIGN KEY (`id_rezervacije`) REFERENCES `rezervacija` (`id_rezervacije`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
